6cb9944de6b0752073251bc69ed4b58cf7db5e61
oscarrazuri <oscar_razuri@hotmail.com>
2021-11-11 06:58:07 +0000

    Add myCity.py file to devasc-study-team

---
+++ myCity.py
@@ -0,0 +1,10 @@
+#Definiendo la función myCity con el argumento city
+def myCity(city):
+    print ("I live in "+ city + ".")
+
+#Lamando a la función myCity
+myCity("Austin")
+myCity("Tokio")
+myCity("Salzburgo")
+
+    
